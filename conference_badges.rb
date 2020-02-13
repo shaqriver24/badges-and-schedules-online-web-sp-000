0d1b19e70e06d14ef01fc8bc15b90928b9783f1a
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-13 08:18:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,15 +3,15 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(arr)
+def batch_badge_creator(attendees)
   new_arr = []
-  arr.each { |name| new_arr << badge_maker(name)}
+  attendees.each { |name| new_arr << badge_maker(name)}
   new_arr
 end
 
-def assign_rooms(arr)
+def assign_rooms(attendees)
   new_arr = []
-  arr.each_with_index { |name, idx| new_arr << "Hello, #{name}! You'll be assigned to room #{idx + 1}!"}
+  attendees.each_with_index { |name, idx| new_arr << "Hello, #{name}! You'll be assigned to room #{idx + 1}!"}
   new_arr
 end
 
