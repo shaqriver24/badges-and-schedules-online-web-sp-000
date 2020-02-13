fb4221a5ce2856dbdf2d7a026342c337cb8be5ac
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-13 08:22:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,5 +17,5 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each { |badge| puts badge }
-  assign_rooms.each { |room| puts room }
+  assign_rooms(attendees).each { |room| puts room }
 end
