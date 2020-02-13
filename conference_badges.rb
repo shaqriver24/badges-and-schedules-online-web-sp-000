4dfd8bb3793ad95fb4cad5482411eaa0d2d6a68f
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-13 08:16:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,6 @@ end
 
 def assign_rooms(arr)
   new_arr = []
-  arr.each_with_index { |name, idx| new_arr << "Hello, #{name}! You'll be assigned to room #{idx + 1}"}
+  arr.each_with_index { |name, idx| new_arr << "Hello, #{name}! You'll be assigned to room #{idx + 1}!"}
   new_arr
 end
